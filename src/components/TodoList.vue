cf8dd8641dccb0af53522e97c0e2f1bf8697737b
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-10-30 13:00:09 +0900

    add:Create / Delete function for TodoList

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,42 +1,53 @@
 <template>
   <h1>{{ title }}</h1>
   <input type="text" v-model="newList"/>
-  <button @click="addLists">追加する</button>
+  <button @click.prevent="addTodo">追加する</button>
+  <button @click="deleteTodo">削除</button>
 
-  <li v-for="(list, index) in lists" :key="index">
-<!--    <input type="text" />-->
-    <span>{{ list.text }}</span>
-    <button @click="deleteLists">削除</button>
-  </li>
+
+  <p v-if="lists.length === 0">ToDoがありません！</p>
+  <ul v-else>
+    <li v-for="(list, index) in lists" :key="index">
+      <input type="checkbox" v-model="list.isDone" />
+      <span :class="{'list-done':list.isDone }">{{ list.text }}</span>
+      <button @click="editTodo = true">編集</button>
+    </li>
+  </ul>
+
 </template>
+
+<!--<template v-model="editTodo">-->
+<!--  ダイアログ-->
+<!--</template>-->
+
 
 <script>
 export default {
   name: "TodoList",
   data(){
     return{
-      newList:"",
-      list: [],
-      lists: [{
-        text: 'やること1',
-      },
-        {
-          text: 'やること2',
-        },
-
-      ]
+      newList: '',
+      lists: [],
+      editTodo: false,
     }
   },
   methods:{
-    addLists(){
+    addTodo(){
       if(!this.newList){
         alert('文字を入力して下さい')
         return
       }
-      this.list.push({
+      this.lists.push({
+        isDone: false,
         text: this.newList,
       })
       this.newList = ''
+    },
+    // editTodo(){
+    //   console.log('あたり')
+    // },
+    deleteTodo(){
+      this.lists = this.lists.filter((list) => !list.isDone)
     },
   },
   props:{
@@ -46,5 +57,11 @@ export default {
 </script>
 
 <style scoped>
+.list-done {
+  text-decoration: line-through;
+}
 
+body {
+  background-color: rgba(16, 206, 1, 0.98);
+}
 </style>

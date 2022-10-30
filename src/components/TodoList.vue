873c18b81359c84ed01ce45fdeedf2494dbed763
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-10-30 10:38:29 +0900

    wip

---
+++ src/components/TodoList.vue
@@ -0,0 +1,50 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" v-model="newList"/>
+  <button @click="addLists">追加する</button>
+
+  <li v-for="(list, index) in lists" :key="index">
+<!--    <input type="text" />-->
+    <span>{{ list.text }}</span>
+    <button @click="deleteLists">削除</button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  data(){
+    return{
+      newList:"",
+      list: [],
+      lists: [{
+        text: 'やること1',
+      },
+        {
+          text: 'やること2',
+        },
+
+      ]
+    }
+  },
+  methods:{
+    addLists(){
+      if(!this.newList){
+        alert('文字を入力して下さい')
+        return
+      }
+      this.list.push({
+        text: this.newList,
+      })
+      this.newList = ''
+    },
+  },
+  props:{
+    title:String,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>

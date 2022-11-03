166e7090d63b050a48c52f6ac04ccc70f2accb09
masayuki201 <masayuki201_201@yahoo.co.jp>
2022-11-03 17:06:47 +0900

    add: Search function for todoList

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,34 +2,35 @@
   <h1>{{ title }}</h1>
   <input type="text" v-model="newList" placeholder="内容"/>
   <button class="addBtn" @click.prevent="addTodo">追加</button>
-  <button  class="deleteBtn" @click="deleteTodo">削除</button>
+  <button class="deleteBtn" @click="deleteTodo">削除</button>
+  <br>
+  <input class="searchText" type="text" v-model="keyword" placeholder="検索">
 
-<div class="contents">
-  <p v-if="lists.length === 0" class="warningMessage">Todoがありません！</p>
-  <ul class="contents_ul" v-else>
-<!--    <input type="text" v-model="query">検索-->
-
-    <li class="contents_li" v-for="(list, index) in lists" :key="index">
-      <input type="checkbox" v-model="list.isDone" />
-      <span :class="{'list-done':list.isDone }">{{ list.text }}</span>
-      <div v-if="list.isActive">
+  <div class="contents">
+    <p v-if="lists.length === 0" class="warningMessage">Todoがありません！</p>
+    <ul class="contents_ul" v-else>
+      <li class="contents_li" v-for="(list, index) in filteredLists" :key="index">
+        <input type="checkbox" v-model="list.isDone"/>
+        <span :class="{'list-done':list.isDone }">{{ list.text }}</span>
+        <div v-if="list.isActive">
         <span>
           <input type="text" v-model="list.text">
         </span>
-        <button class="doneBtn" @click="updateDone(index)">完了</button>
-      </div>
-      <button class="editBtn" v-show="!list.isActive" @click="updateTodo(index)">編集</button>
-    </li>
-  </ul>
-</div>
+          <button class="doneBtn" @click="updateDone(index)">完了</button>
+        </div>
+        <button class="editBtn" v-show="!list.isActive" @click="updateTodo(index)">編集</button>
+      </li>
+    </ul>
+  </div>
 
 </template>
 
 <script>
 export default {
   name: "TodoList",
-  data(){
-    return{
+  data() {
+    return {
+      keyword: '',
       newList: '',
       lists: [
         {
@@ -45,10 +46,22 @@ export default {
       editTodo: false,
     }
   },
-  methods:{
+  computed: {
+    filteredLists: function () {
+      const lists = [];
+      for (const i in this.lists) {
+        const list = this.lists[i];
+        if (list.text.indexOf(this.keyword) !== -1) {
+          lists.push(list);
+        }
+      }
+      return lists;
+    }
+  },
+  methods: {
     //追加
-    addTodo(){
-      if(!this.newList){
+    addTodo() {
+      if (!this.newList) {
         alert('文字を入力して下さい')
         return
       }
@@ -59,21 +72,21 @@ export default {
       this.newList = ''
     },
     //編集
-    updateTodo(index){
+    updateTodo(index) {
       this.lists[index].isActive = true
       this.lists[index].text = this.list[index].text
     },
     //完了
-    updateDone(index){
+    updateDone(index) {
       this.lists[index].isActive = false
     },
     //削除
-    deleteTodo(){
+    deleteTodo() {
       this.lists = this.lists.filter((list) => !list.isDone)
     },
   },
-  props:{
-    title:String,
+  props: {
+    title: String,
   },
 }
 </script>
@@ -99,6 +112,10 @@ export default {
   text-align: left;
 }
 
+.searchText {
+  margin: 5px;
+}
+
 .addBtn {
   background-color: deepskyblue;
   margin: 5px;
@@ -106,7 +123,7 @@ export default {
 
 .deleteBtn {
   background-color: red;
-  color:white;
+  color: white;
   margin: 5px;
 }
 
